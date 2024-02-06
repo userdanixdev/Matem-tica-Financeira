bb6e1d51dd1b01a4880beae7d1df3b72a8eeb86c
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-02-06 12:29:57 -0300

    Create Taxas Equivalentes - J.C

---
+++ Taxas Equivalentes - J.C
@@ -0,0 +1,16 @@
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+TAXAS PROPORCIONAIS SERVEM PARA JUROS SIMPLES
+TAXA EQUIVALENTES PARA JUROS COMPOSTOS
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+i = 8 % a.s para ???? % a.a
+k ----> 2 semestres ( períodos ) em 1 ano.
+
+Fórmula para usar a taxa equivalente de juros compostos:
+    - 1+ I = (1+ i)^k
+    - 1+ i = (1+0,08)²
+    - 1 + I = 1,1664
+- I = 0,1664 ( em termos unitários para transformar em percentuais: 16,64% )
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
