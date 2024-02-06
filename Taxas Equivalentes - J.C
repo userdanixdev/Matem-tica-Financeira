06486ddbb95434e43169547e3e770eebf1e49b98
Daniel Martins <132594952+userdanixdev@users.noreply.github.com>
2024-02-06 12:34:57 -0300

    Update Taxas Equivalentes - J.C

--- Taxas Equivalentes - J.C
+++ Taxas Equivalentes - J.C
@@ -11,6 +11,18 @@ Fórmula para usar a taxa equivalente de juros compostos:
     - 1 + I = 1,1664
 - I = 0,1664 ( em termos unitários para transformar em percentuais: 16,64% )
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Outro Exemplo:
+    - i = 3% a.b ----> ???? % a.s
+% a.b ----> i ( são necessários 3 bimestres para completar 1 semestre )
+% a.s ----> I
+k --------> 3
+    -  1 + I = (1+ i)^k
+    -  1 + I = (1+ 0,03)^³
+    -  1 + I = 1,092727
+    - I = 1,092727 - 1
+    - I = 0,092727
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 
 
